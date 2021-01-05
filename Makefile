4a64119fd5165b9e493b3f326c804fd3e3d1ef4a
marf <marvin@proudinnovations.com>
2021-01-05 15:14:23 +0100

    Added Makefile, so that makes releasing more easy
    Signed-off-by: marf <git@marfprojects.nl>

---
+++ Makefile
@@ -0,0 +1,9 @@
+run:
+	go run main.go
+
+build:
+	go build -o bin/Modpack-Updater main.go
+
+build-all:
+	./scripts/buildall.sh
+
---
+++ scripts/buildall.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+#
+# GoLang cross-compile snippet for Go 1.6+ based loosely on Dave Chaney's cross-compile script:
+# http://dave.cheney.net/2012/09/08/an-introduction-to-cross-compilation-with-go
+#
+
+PLATFORMS="darwin/amd64"
+PLATFORMS="$PLATFORMS windows/amd64 windows/386"
+PLATFORMS="$PLATFORMS linux/amd64 linux/386"
+PLATFORMS="$PLATFORMS freebsd/amd64"
+PLATFORMS="$PLATFORMS netbsd/amd64"
+PLATFORMS="$PLATFORMS openbsd/amd64"
+
+# Minecraft is not really supported on ARM, but it does run. so why not.
+PLATFORMS_ARM="linux"
+
+type setopt >/dev/null 2>&1
+
+SCRIPT_NAME=`basename "$0"`
+FAILURES=""
+SOURCE_FILE=`echo $@ | sed 's/\.go//'`
+CURRENT_DIRECTORY=${PWD##*/}
+OUTPUT=bin/${SOURCE_FILE:-$CURRENT_DIRECTORY}
+
+for PLATFORM in $PLATFORMS; do
+  GOOS=${PLATFORM%/*}
+  GOARCH=${PLATFORM#*/}
+  BIN_FILENAME="${OUTPUT}-${GOOS}-${GOARCH}"
+  if [[ "${GOOS}" == "windows" ]]; then BIN_FILENAME="${BIN_FILENAME}.exe"; fi
+  CMD="GOOS=${GOOS} GOARCH=${GOARCH} go build -o ${BIN_FILENAME} $@"
+  echo "${CMD}"
+  eval $CMD || FAILURES="${FAILURES} ${PLATFORM}"
+done
+
+# ARM builds
+if [[ $PLATFORMS_ARM == *"linux"* ]]; then
+  CMD="GOOS=linux GOARCH=arm64 go build -o ${OUTPUT}-linux-arm64 $@"
+  echo "${CMD}"
+  eval $CMD || FAILURES="${FAILURES} ${PLATFORM}"
+fi
+for GOOS in $PLATFORMS_ARM; do
+  GOARCH="arm"
+  # build for each ARM version
+  for GOARM in 7; do
+    BIN_FILENAME="${OUTPUT}-${GOOS}-${GOARCH}${GOARM}"
+    CMD="GOARM=${GOARM} GOOS=${GOOS} GOARCH=${GOARCH} go build -o ${BIN_FILENAME} $@"
+    echo "${CMD}"
+    eval "${CMD}" || FAILURES="${FAILURES} ${GOOS}/${GOARCH}${GOARM}"
+  done
+done
+
+# eval errors
+if [[ "${FAILURES}" != "" ]]; then
+  echo ""
+  echo "${SCRIPT_NAME} failed on: ${FAILURES}"
+  exit 1
+fi
+
